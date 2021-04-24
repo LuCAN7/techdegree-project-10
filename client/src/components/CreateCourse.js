6919027891608f886973ab64a6bb4f03b4a626a9
Lucan F <dapps911@hotmail.com>
2021-04-24 17:22:06 -0400

    set a state value for all inputs

--- client/src/components/CreateCourse.js
+++ client/src/components/CreateCourse.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 
 const CreateCourse = () => {
   // *--Todo--*
-  // []-set a state value for inputs
+  // [x]-set a state value for inputs
   // []-define handleUpdate function to 'POST' to db
   // []-console log the response object
+
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [description, setDescription] = useState('');
+  const [estimate, setEstimate] = useState('');
+  const [materialsNeeded, setMaterialsNeeded] = useState('');
 
   const handleUpdate = (e) => {
     e.preventDefault();
@@ -32,7 +37,8 @@ const CreateCourse = () => {
                 id='courseTitle'
                 name='courseTitle'
                 type='text'
-                // value={title}
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
               />
 
               <label HtmlFor='courseAuthor'>Course Author</label>
@@ -40,13 +46,16 @@ const CreateCourse = () => {
                 id='courseAuthor'
                 name='courseAuthor'
                 type='text'
-                // value={author}
+                value={author}
+                onChange={(e) => setAuthor(e.target.value)}
               />
 
               <label HtmlFor='courseDescription'>Course Description</label>
               <textarea
                 id='courseDescription'
                 name='courseDescription'
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
               ></textarea>
             </div>
             <div>
@@ -55,11 +64,17 @@ const CreateCourse = () => {
                 id='estimatedTime'
                 name='estimatedTime'
                 type='text'
-                // value={estimate}
+                value={estimate}
+                onChange={(e) => setEstimate(e.target.value)}
               />
 
               <label HtmlFor='materialsNeeded'>Materials Needed</label>
-              <textarea id='materialsNeeded' name='materialsNeeded'></textarea>
+              <textarea
+                id='materialsNeeded'
+                name='materialsNeeded'
+                value={materialsNeeded}
+                onChange={(e) => setMaterialsNeeded(e.target.value)}
+              ></textarea>
             </div>
           </div>
           <button className='button' type='submit'>
